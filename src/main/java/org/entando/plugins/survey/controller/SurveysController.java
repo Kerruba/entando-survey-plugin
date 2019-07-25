6325f28abb90ebe5b65ef68b0f1522a085ec1369
Sérgio Marcelino <sergiofilhow@gmail.com>
2019-07-25 13:02:43 -0300

    adding a more standardised way to return the objects via REST API

--- src/main/java/org/entando/plugins/survey/controller/SurveysController.java
+++ src/main/java/org/entando/plugins/survey/controller/SurveysController.java
@@ -9,11 +9,10 @@ import org.entando.plugins.survey.dto.request.CreateSurveyRequest;
 import org.entando.plugins.survey.dto.request.SubmitSurveyRequest;
 import org.entando.plugins.survey.dto.submission.SurveySubmissionDto;
 import org.entando.plugins.survey.dto.survey.SurveyDto;
-import org.entando.plugins.survey.model.BasePage;
-import org.entando.plugins.survey.model.BasePageable;
-import org.entando.plugins.survey.model.Survey;
-import org.entando.plugins.survey.model.SurveySubmission;
 import org.entando.plugins.survey.service.SurveyService;
+import org.entando.web.request.PagedListRequest;
+import org.entando.web.response.PagedRestResponse;
+import org.entando.web.response.SimpleRestResponse;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.entando.plugins.survey.controller.AuthPermissions.SURVEY_CREATE;
 import static org.entando.plugins.survey.controller.AuthPermissions.SURVEY_GET;
@@ -46,75 +43,57 @@ public class SurveysController {
     @Secured(SURVEY_GET)
     @ApiOperation(notes = "Gets a survey", nickname = "getSurvey", value = "GET Survey")
     @GetMapping(path = "/{uuid}", produces = { APPLICATION_JSON_VALUE })
-    public SurveyDto get(@PathVariable final String uuid) {
+    public SimpleRestResponse<SurveyDto> get(@PathVariable final String uuid) {
         log.info("Requesting survey with ID {}", uuid);
-        return SurveyDto.fromModel(surveyService.get(uuid));
+        return new SimpleRestResponse<>(SurveyDto.fromModel(surveyService.get(uuid)));
     }
 
     @Secured(SURVEY_CREATE)
     @ApiOperation(notes = "Creates a survey", nickname = "createSurvey", value = "CREATE Survey")
     @PostMapping(produces = { APPLICATION_JSON_VALUE })
-    public SurveyDto create(final @Valid @RequestBody CreateSurveyRequest request) {
-        log.info("Creating survey");
-        return SurveyDto.fromModel(surveyService.create(request));
+    public SimpleRestResponse<SurveyDto> create(final @Valid @RequestBody CreateSurveyRequest request) {
+        log.info("Creating survey {} with request {}", request, request);
+        return new SimpleRestResponse<>(SurveyDto.fromModel(surveyService.create(request)));
     }
 
     @Secured(SURVEY_LIST)
     @ApiOperation(notes = "Lists all surveys", nickname = "listSurveus", value = "LIST Survey")
     @GetMapping(produces = { APPLICATION_JSON_VALUE })
-    public BasePage<SurveyDto> list(final BasePageable page) {
-        log.info("Listing surveys");
-        List<Survey> result = surveyService.list(page);
-        List<SurveyDto> dtos = new ArrayList<>();
-        for (Survey survey : result) {
-            dtos.add(SurveyDto.fromModel(survey));
-        }
-
-        return BasePage.<SurveyDto>builder()
-                .list(dtos)
-                .page(page.getPage())
-                .pageSize(page.getPageSize()) //TODO Possible bug on page count generation?
-                .build();
+    public PagedRestResponse<SurveyDto> list(final PagedListRequest restListRequest) {
+        log.info("Listing surveys {}", restListRequest);
+        return surveyService.list(restListRequest)
+                .map(SurveyDto::fromModel)
+                .toRestResponse();
     }
 
     @Secured(SURVEY_SUBMISSION_LIST)
     @ApiOperation(notes = "Lists a survey submissions", nickname = "listSurveySubmissions", value = "LIST SurveySubmission")
     @GetMapping(path = "/{uuid}/submissions", produces = { APPLICATION_JSON_VALUE })
     //TODO support query filters
-    public BasePage<SurveySubmissionDto> listSubmissions(@PathVariable final String uuid, final BasePageable page) {
-        log.info("Lists survey submissions for survey ID {}", uuid);
-
-
-        List<SurveySubmission> result = surveyService.listSubmissions(uuid, page);
-        List<SurveySubmissionDto> dtos = new ArrayList<>();
-        for (SurveySubmission submission : result) {
-            dtos.add(SurveySubmissionDto.fromModel(submission));
-        }
-
-        return BasePage.<SurveySubmissionDto>builder()
-                .list(dtos)
-                .page(page.getPage())
-                .pageSize(page.getPageSize()) //TODO Possible bug on page count generation?
-                .build();
+    public PagedRestResponse<SurveySubmissionDto> listSubmissions(@PathVariable final String uuid, final PagedListRequest listRequest) {
+        log.info("Listing survey submissions for survey ID {} and request {}", uuid, listRequest);
+        return surveyService.listSubmissions(uuid, listRequest)
+                .map(SurveySubmissionDto::fromModel)
+                .toRestResponse();
     }
 
     @Secured(SURVEY_SUBMISSION_CREATE)
     @ApiOperation(notes = "Submits a survey", nickname = "createSurveySubmission", value = "CREATE SurveySubmission")
     @PostMapping(path = "/{uuid}/submissions", produces = { APPLICATION_JSON_VALUE })
-    public SurveySubmissionDto submitSurvey(@PathVariable final String uuid, @Valid @RequestBody final SubmitSurveyRequest request) {
-        log.info("Submitting survey with ID {}", uuid);
-        return SurveySubmissionDto.fromModel(
-                surveyService.submit(uuid, request));
+    public SimpleRestResponse<SurveySubmissionDto> submitSurvey(
+            @PathVariable final String uuid, @Valid @RequestBody final SubmitSurveyRequest request) {
+        log.info("Submitting survey with ID {} and request {}", uuid, request);
+        return new SimpleRestResponse<>(SurveySubmissionDto.fromModel(surveyService.submit(uuid, request)));
     }
 
     @Secured(SURVEY_SUBMISSION_GET)
     @ApiOperation(notes = "Gets a survey submission", nickname = "getSurveySubmission", value = "GET SurveySubmission")
     @GetMapping(path = "/{uuid}/submissions/{submissionUuid}", produces = { APPLICATION_JSON_VALUE })
-    public SurveySubmissionDto getSubmission(@PathVariable final String uuid, @PathVariable final String submissionUuid) {
+    public SimpleRestResponse<SurveySubmissionDto> getSubmission(
+            @PathVariable final String uuid, @PathVariable final String submissionUuid) {
         log.info("Requesting survey submission with ID {} and survey ID {}", submissionUuid, uuid);
-        new SurveySubmissionDto();
-        return SurveySubmissionDto.fromModel(
-                surveyService.getSubmission(uuid, submissionUuid));
+        return new SimpleRestResponse<>(SurveySubmissionDto.fromModel(
+                surveyService.getSubmission(uuid, submissionUuid)));
     }
 
 }
--- src/main/java/org/entando/plugins/survey/service/SurveyService.java
+++ src/main/java/org/entando/plugins/survey/service/SurveyService.java
@@ -6,7 +6,6 @@ import org.entando.plugins.survey.dto.request.SubmitSurveyRequest;
 import org.entando.plugins.survey.exception.SurveyNotFoundException;
 import org.entando.plugins.survey.exception.SurveySubmissionNotFoundException;
 import org.entando.plugins.survey.model.Answer;
-import org.entando.plugins.survey.model.BasePageable;
 import org.entando.plugins.survey.model.Survey;
 import org.entando.plugins.survey.model.SurveySubmission;
 import org.entando.plugins.survey.model.answer.AnswerList;
@@ -17,7 +16,11 @@ import org.entando.plugins.survey.repository.answer.AnswerRateRepository;
 import org.entando.plugins.survey.repository.answer.AnswerTextRepository;
 import org.entando.plugins.survey.repository.survey.SurveyRepository;
 import org.entando.plugins.survey.repository.survey.SurveySubmissionRepository;
-import org.springframework.data.domain.PageRequest;
+import org.entando.web.jpa.PagedListRequestDataUtils;
+import org.entando.web.request.PagedListRequest;
+import org.entando.web.response.PagedMetadata;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,9 +49,10 @@ public class SurveyService {
         return surveyRepository.save(request.getModel());
     }
 
-    public List<Survey> list(BasePageable page) {
-        return surveyRepository.findAll(PageRequest.of(page.getPage(), page.getPageSize()))
-                .getContent();
+    public PagedMetadata<Survey> list(final PagedListRequest listRequest) {
+        final Pageable pageable = PagedListRequestDataUtils.toPageable(listRequest);
+        final Page<Survey> page = surveyRepository.findAll(pageable);
+        return new PagedMetadata<>(listRequest, page.getContent(), (int) page.getTotalElements());
     }
 
     public SurveySubmission submit(String uuid, SubmitSurveyRequest request) {
@@ -82,9 +86,10 @@ public class SurveyService {
         return surveySubmissionRepository.findByIdAndSurveyId(UUID.fromString(submissionUuid), UUID.fromString(surveyUuid))
                 .orElseThrow(SurveySubmissionNotFoundException::new);
     }
-    
-    public List<SurveySubmission> listSubmissions(String uuid, BasePageable page) {
-        return surveySubmissionRepository.findAll(PageRequest.of(page.getPage(), page.getPageSize()))
-                .getContent();
+
+    public PagedMetadata<SurveySubmission> listSubmissions(String uuid, PagedListRequest request) {
+        final Pageable pageable = PagedListRequestDataUtils.toPageable(request);
+        final Page<SurveySubmission> page = surveySubmissionRepository.findAll(pageable);
+        return new PagedMetadata<>(request, page.getContent(), (int) page.getTotalElements());
     }
 }
